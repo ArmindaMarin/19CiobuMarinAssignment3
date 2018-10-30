e764af8c9ce88fc3239a6908fe411e5e383361fe
Arminda Marin <arminda_marin19@hotmail.com>
2018-10-30 13:30:40 +0100

    test me

--- src/Application.java
+++
@@ -1,9 +0,0 @@
-public class Application {
-    public static void main(String[] args) {
-        new Application().run();
-    }
-
-    public void run(){
-        
-    }
-}
---
+++ src/SalesRep.java
@@ -0,0 +1,12 @@
+import akka.actor.AbstractActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
+public class SalesRep extends AbstractActor {
+    private final LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
+
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
+}
